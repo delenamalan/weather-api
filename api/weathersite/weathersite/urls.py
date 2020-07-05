4bc2d9da05c0c70ae7b8d03eb9a8410985108f6e
Delena Malan <delena.malan@gmail.com>
2020-07-05 10:47:34 +0200

    Add api app

--- api/weathersite/weathersite/urls.py
+++ api/weathersite/weathersite/urls.py
@@ -1,4 +1,4 @@
-"""weather URL Configuration
+"""weathersite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls'))
 ]
--- readme.md
+++ readme.md
@@ -4,13 +4,21 @@
 
 ### API
 
+Dependencies:
+- Python 3.6
+- SQLite (or change the settings to use a different database)
+
 ```
 cd api
 virtualenv -p /usr/bin/python3.6 venv && source venv/bin/activate
 pip install -r requirements.txt
-cd weather
+cd weathersite
+python manage.py migrate
+python manage.py createsuperuser
 python manage.py runserver
 ```
 
 ### Frontend
 
+
+## Routes
