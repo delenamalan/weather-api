f2a5bfcb2ae7d520bc55631945ec5f4a7ab275f9
Delena Malan <delena.malan@gmail.com>
2020-07-06 09:48:57 +0200

    Uninstall Django REST framework

--- api/weathersite/settings.py
+++ api/weathersite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -113,15 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
-}
 
 WEATHER_CLASS = "weather.metaweather.MetaWeather"
--- api/weathersite/urls.py
+++ api/weathersite/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    url(r"^api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
 ]
